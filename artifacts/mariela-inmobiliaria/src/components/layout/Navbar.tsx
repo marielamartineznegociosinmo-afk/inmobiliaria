27153f340fce6067a1b4854b62b2bfad7aac50a7
RodrigoMignola <mignolarodrigo@gmail.com>
2026-07-02 12:11:54 -0300

    Fix imagenes del navbar y footer

--- artifacts/mariela-inmobiliaria/src/components/layout/Navbar.tsx
+++ artifacts/mariela-inmobiliaria/src/components/layout/Navbar.tsx
@@ -21,7 +21,7 @@ export function Navbar() {
         <Link href="/" className="flex items-center gap-2">
           <div className="flex flex-col">
               <img
-              src="/Logo-Navbar.png"
+              src="/Logo-navbar.png"
               alt="Logo"
               className="h-15 w-60"
             />
